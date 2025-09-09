e67f58b8ab498f761417b4c3bbede182ebe58f31
RickM95 <remerren@hotmail.com>
2025-09-09 14:34:02 -0600

    cocina js promesa

---
+++ js/edmund.js
@@ -0,0 +1,23 @@
+function cocinar(comida) {
+    console.log(`Cocinando ${comida}...`);
+
+    const tiempos = {
+        'pizza': 3000,
+        'hamburguesa': 2000,
+        'ensalada': 1000
+    };
+
+    const tiempo = tiempos[comida.toLowerCase()];
+
+    if (!tiempo) {
+        return Promise.reject(new Error(`No podemos cocinar ${comida}`));
+    }
+
+    // Retornar una promesa que se resuelve después del tiempo de cocción
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`${comida} lista en ${tiempo/1000} segundos`);
+            resolve(comida);
+        }, tiempo);
+    });
+}
